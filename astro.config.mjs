54a5accb14a15c72bbd4e1b76de14620460471ee
2uent1n <127933396+2uent1n@users.noreply.github.com>
2026-02-19 01:28:16 +0100

    chore: add oxfmt + oxlint, restrict Prettier to .astro files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,25 @@ import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-import starlightThemeNext from 'starlight-theme-next'
+import starlightThemeNext from 'starlight-theme-next';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'Cadensio Docs',
-        sidebar: [
-            {
-                label: 'Getting Started',
-                link: '/getting-started',
-            }
-        ],
-        plugins: [starlightThemeNext()],
-        customCss: ['./src/styles/global.css'],
-		}), react()],
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  integrations: [
+    starlight({
+      title: 'Cadensio Docs',
+      sidebar: [
+        {
+          label: 'Getting Started',
+          link: '/getting-started',
+        },
+      ],
+      plugins: [starlightThemeNext()],
+      customCss: ['./src/styles/global.css'],
+    }),
+    react(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
